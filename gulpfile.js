9607fd53950891dc14e7348bef316e37aa77d951
Nikolay Troshkov <ariser@ariser.ru>
2016-09-26 05:41:10 +0300

    update gulpfile & npm

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 var gulp          = require('gulp'),
     runSequence   = require('run-sequence'),
+    plumber       = require('gulp-plumber'),
 
     webpack       = require('webpack-stream'),
     webpackConfig = require('./webpack.config.js'),
@@ -14,7 +15,8 @@ var config = {
 			scripts: 'src/assets/scripts',
 			styles:  'src/assets/styles',
 			images:  'src/assets/images',
-			fonts:   'src/assets/fonts'
+			fonts:   'src/assets/fonts',
+			audio:   'src/assets/audio'
 		},
 		build: {
 			root:   'public',
@@ -23,10 +25,19 @@ var config = {
 			js:     'public/assets/js',
 			css:    'public/assets/css',
 			images: 'public/assets/img',
-			fonts:  'public/assets/fonts'
+			fonts:  'public/assets/fonts',
+			audio:  'public/assets/audio'
 		}
 	}
 };
+
+gulp.task('audio:clean', function () {
+	return del([config.paths.build.audio + '/**']);
+});
+gulp.task('audio', function () {
+	return gulp.src(config.paths.src.audio + '/**')
+		.pipe(gulp.dest(config.paths.build.audio));
+});
 
 gulp.task('views:clean', function () {
 	return del([config.paths.build.views + '/**']);
@@ -52,18 +63,19 @@ gulp.task('scripts:vendor', function () {
 		.pipe(gulp.dest(config.paths.build.js + '/vendor'));
 });
 gulp.task('webpack', function () {
-	return gulp.src(config.paths.src.scripts + '/index.js')
+	return gulp.src('')
+		.pipe(plumber())
 		.pipe(webpack(webpackConfig))
 		.pipe(gulp.dest(config.paths.build.js));
 });
 gulp.task('scripts', ['scripts:vendor', 'webpack']);
 
-gulp.task('clean', ['views:clean', 'styles:clean', 'scripts:clean']);
+gulp.task('clean', ['views:clean', 'styles:clean', 'scripts:clean', 'audio:clean']);
 
 gulp.task('build', function (next) {
 	runSequence(
 		'clean',
-		['views', 'styles', 'scripts'],
+		['views', 'styles', 'scripts', 'audio'],
 		next
 	);
 });
